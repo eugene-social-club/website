742bc37a3ce0199b4bad67c71437112f1380da51
Joshua <josh.goss@kajabi.com>
2024-09-01 23:06:07 -0700

    feat: Adds events api

---
+++ src/server/discord_controller.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import { getGuildScheduledEvents } from './utils/discord_utils';
+
+const app = express();
+
+app.get('/scheduled_events', async function (req, res) {
+    console.log('Getting scheduled events');
+    console.log('Guild ID:', process.env.GUILD_GANG_ID);
+    const guild_id = "1033252271582085172";
+    const events = await getGuildScheduledEvents(guild_id);
+    console.log('Events:', events);
+    return res.json(events);
+});
